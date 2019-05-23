85917b31ed3fad9b61e0815bd57fd7a616dfcd47
Qianqian Li <Qianqian.Li@microsoft.com>
2019-05-23 11:58:18 +0800

    init

---
+++ src/webparts/singlePageApp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISinglePageAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SinglePageAppWebPartStrings' {
+  const strings: ISinglePageAppWebPartStrings;
+  export = strings;
+}
